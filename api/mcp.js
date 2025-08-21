eb714867be5535d6dc32a8203dcda3103ac81902
paoloricciuti <ricciutipaolo@gmail.com>
2025-08-21 15:38:19 +0200

    fix: add options

--- api/mcp.js
+++ api/mcp.js
@@ -59,6 +59,13 @@ export async function GET(request) {
 	);
 }
 
+export async function OPTIONS(request) {
+	return (
+		(await transport.respond(request)) ??
+		new Response('Not Found', { status: 404 })
+	);
+}
+
 export async function POST(request) {
 	return (
 		(await transport.respond(request)) ??
